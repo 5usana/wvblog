7b4e03ab3713aed7e581d19b18c15d82c47fa48a
Susana Martins <scmartins87@gmail.com>
2021-04-20 13:07:08 -0700

    added app.css for hamburger menu, uncertain if i'll keep it

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 
 
 
+
 function NavBar() {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     const handleToggle = () => {
-        setNavbarOpen(prev => !prev)
+        setNavbarOpen(!navbarOpen)
     };
 
     return (
         <nav className='navBar'>
             <h1>WV</h1>
             <button onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
-            <ul>
+            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
 
             </ul>
 

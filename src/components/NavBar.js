c089ce69d0703d332889425cca39b70186c5f886
Susana Martins <scmartins87@gmail.com>
2021-04-21 10:06:40 -0700

    navbar updated with links to different components, removed hambuger menu <- not working out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react';
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function NavBar() {
-    const [navbarOpen, setNavbarOpen] = useState(false);
+  
 
-    const handleToggle = () => {
-        setNavbarOpen(!navbarOpen)
-    };
 
     return (
         <nav className='navBar'>
-            <h1>WV</h1>
-            <button onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
-            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+            <ul>
+                <Link to='/'>
+					<button
+						className='Home'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						WV
+					</button>
+				</Link>
+                <Link to='/about'>
+					<button
+						className='primary'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						About
+					</button>
+				</Link>
+                <Link to='/post'>
+					<button
+						className='secondary'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						Post
+					</button>
+				</Link>
+                <Link to='/log'>
+					<button
+						className='log'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						Entries
+					</button>
+				</Link>
 
             </ul>
 

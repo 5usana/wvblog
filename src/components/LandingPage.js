79863327beaebea9a14cb5cdee355d9612883378
Susana Martins <scmartins87@gmail.com>
2021-04-23 08:18:21 -0700

    added styled componenets, moved text in landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import NavBar from './NavBar';
 
+const HeroDiv = styled.div`
+	padding: 160px 0 0 260px;
+	width: 50vw;
+	display: inline-block;
+`;
 
+const HeaderOne = styled.h1`
+	font-size: 3rem;
+	display: inline;
+`;
+const HeaderTwo = styled.h2`
+	font-size: 2rem;
+	display: inline;
+`;
 
 function LandingPage() {
     return (
-        <div className="landingPage">
 
-           <NavBar />
-            <h1>Hey spit it out.</h1>
-            <h2>Don't think, let it flow.</h2>
+        <section>
+        <NavBar />
+        <HeroDiv>
+            <HeaderOne>Hey spit it out. </HeaderOne>
+            <br />
+            <HeaderTwo> Don't think, let it flow.</HeaderTwo>
+        </HeroDiv>
 
-        </div>
+        </section>
     );
 }
 

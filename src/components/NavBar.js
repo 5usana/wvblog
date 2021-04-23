eeaf3b2070554f922504de2d9920564964cee1c2
Susana Martins <scmartins87@gmail.com>
2021-04-23 10:04:02 -0700

    updated navbar with some styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,65 +13,56 @@ const HomeNav = styled.nav`
 // 	color: red;
 // `;
 
-
 function NavBar() {
 
-
-
     return (
-        <nav className='navBar'>
-            <ul>
-				<HomeNav>
-					<Link to='/signin'>
-						<button
-							className='SignIn'
-							width='348px'
-							size='1.5rem'
-							margin='0'
-							height='56px'>
-							Sign In
-						</button>
-					</Link>
-					<Link to='/signup'>
-						<button
-							className='SignUp'
-							width='348px'
-							size='1.5rem'
-							margin='0'
-							height='56px'>
-							Sign Up
-						</button>
-					</Link>
-					<Link to='/blog'>
-						<button
-							className='log'
-							width='348px'
-							size='1.5rem'
-							margin='0'
-							height='56px'>
-							Blog
-						</button>
-					</Link>
-					<Link to='/about'>
-						<button
-							className='primary'
-							width='348px'
-							size='1.5rem'
-							margin='0'
-							height='56px'>
-							About
-						</button>
-					</Link>
-				</HomeNav>
-				<Link to='/'>
-						WV
+		<div>
+			<HomeNav>
+				<Link to='/signin'>
+					<button
+						className='SignIn'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						Sign In
+					</button>
 				</Link>
-
-            </ul>
-
-            
-            
-        </nav>
+				<Link to='/signup'>
+					<button
+						className='SignUp'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						Sign Up
+					</button>
+				</Link>
+				<Link to='/blog'>
+					<button
+						className='log'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						Blog
+					</button>
+				</Link>
+				<Link to='/about'>
+					<button
+						className='primary'
+						width='348px'
+						size='1.5rem'
+						margin='0'
+						height='56px'>
+						About
+					</button>
+				</Link>
+			</HomeNav>
+			<Link to='/'>
+				WV
+			</Link>
+		</div>
     );
 }
 

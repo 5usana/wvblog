3ace48cb293d8d7cf1ba16a9334271819042a92e
Susana Martins <scmartins87@gmail.com>
2021-04-24 12:47:45 -0700

    navbar buttons resized to fit mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,52 +3,40 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const HomeNav = styled.nav`
-margin: 0 auto;
-padding: 16px 24px;
-height: 200px;
-font-size: 3rem;
+	margin: 0 auto;
+	padding: 16px 24px;
+	height: 200px;
+	font-size: 3rem;
 `;
 
 const NavContainer = styled.section`
-display: flex;
-flex-direction: row;
-order: 1;
-background-color: #CCE4E5;
-flex-flow: row;	
-
+	display: flex;
+	flex-direction: row;
+	order: 1;
+	background-color: #CCE4E5;
+	flex-flow: row;	
 `;
 
 const HeaderOne = styled.h1`
-	// background-color: coral;
 	font-weight: 100;
 	font-size: 50px;
 	margin-left: 20px;
 	color: black;
 	text-decoration: none;
-
-    
-	// .nav-tag {
-	// 	color: red;
-	// 	font-weight: 100;
-	// 	text-decoration: none;
-	// }
 `;
 
 const SignInButton = styled.button`
 	display:inline-block;
- 	padding:0.35em 1em;
+ 	padding:0.35em .2em;
  	border:0.29em solid #FFFFFF;
  	margin:0 0.6em 0.3em 0;
  	order-radius:0.12em;
-//  	box-sizing: border-box;
-//  	font-weight:400;
-//  	color:#000000;
  	transition: all 0.2s;
 `;
 
 const SignUpButton = styled.button`
 	display:inline-block;
- 	padding:0.35em 1em;
+	padding:0.35em .2em;
  	border:0.29em solid #FFFFFF;
  	margin:0 0.6em 0.3em 0;
  	order-radius:0.12em;
@@ -57,7 +45,7 @@ const SignUpButton = styled.button`
 
 const BlogButton = styled.button`
 	display:inline-block;
- 	padding:0.35em 1em;
+	padding:0.35em .4em;
  	border:0.29em solid #FFFFFF;
  	margin:0 0.6em 0.3em 0;
  	order-radius:0.12em;
@@ -66,9 +54,9 @@ const BlogButton = styled.button`
 
 const AboutButton = styled.button`
 	display:inline-block;
- 	padding:0.35em 1em;
+	padding:0.35em .4em;
  	border:0.29em solid #FFFFFF;
- 	margin:0 0.6em 0.3em 0;
+//  	margin:0 0.6em 0em 0;
  	order-radius:0.12em;
  	transition: all 0.2s;
 `;
@@ -78,53 +66,32 @@ function NavBar() {
     return (
 		<NavContainer>
 			<Link style={{ textDecoration: 'none' }} to='/'>
-				<HeaderOne className='nav-tag' > WV </HeaderOne>
+				<HeaderOne> 
+					WV 
+					</HeaderOne>
 			</Link>
 			<HomeNav>
 				<Link to='/signin'>
-					<SignInButton
-						className='SignIn'
-						width='348px'
-						size='1.5rem'
-						margin='0'
-						height='56px'>
+					<SignInButton>
 						Sign In
 					</SignInButton>
 				</Link>
 				<Link to='/signup'>
-					<SignUpButton
-						className='SignUp'
-						width='348px'
-						size='1.5rem'
-						margin='0'
-						height='56px'>
+					<SignUpButton>
 						Sign Up
 					</SignUpButton>
 				</Link>
 				<Link to='/blog'>
-					<BlogButton
-						className='blog'
-						width='348px'
-						size='1.5rem'
-						margin='0'
-						height='56px'>
+					<BlogButton>
 						Blog
 					</BlogButton>
 				</Link>
 				<Link to='/about'>
-					<AboutButton
-						className='about'
-						width='348px'
-						size='1.5rem'
-						margin='0'
-						height='56px'>
+					<AboutButton>
 						About
 					</AboutButton>
 				</Link>
 			</HomeNav>
-			
-
-			
 		</NavContainer>
     );
 }
